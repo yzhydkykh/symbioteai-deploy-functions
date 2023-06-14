66fe0dd2f0f6eefd0e73305b27079cf031dcb59b
yzhydkykh <yevgen1zhydkykh@gmail.com>
2023-06-14 14:24:57 +0100

    teest

--- install-functions.sh
+++ install-functions.sh
@@ -1,13 +1,6 @@
 #!/bin/sh -l
-
-if [ -z "${FIREBASE_TOKEN}" ]; then
-    echo "FIREBASE_TOKEN is missing"
-    echo '${{ secrets.SERVICE_ACCOUNT_CREDENTIALS }}' > $HOME/gcloud.json
-fi
-
 cd packages/app/
 yarn install
 cd functions
 yarn install
 cd ..
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/gcloud.json && npx firebase-tools deploy --only functions --json
